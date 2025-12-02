607f1bf76c1cd9254a3c4ef8da97eb423d29d6fc
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-02 23:44:12 +0100

    update the footer code

--- apps/ecommerce-frontend/src/app/layout/footer/footer.ts
+++ apps/ecommerce-frontend/src/app/layout/footer/footer.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-footer',
-  imports: [],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
