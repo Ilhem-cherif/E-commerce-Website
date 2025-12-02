91f125e7cad9d8a198923ebe609e99a4053b60e3
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-02 17:53:55 +0100

    change the root prefix

--- apps/ecommerce-frontend/src/app/app.ts
+++ apps/ecommerce-frontend/src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { FaConfig, FaIconLibrary, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FaIconComponent],
+  selector: 'ecom-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'ecommerce-frontend';
+export class App implements OnInit{
+  
+  private faIconsLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconsLibrary.addIcons(...fontAwesomeIcons);
+  }
+  
 }
