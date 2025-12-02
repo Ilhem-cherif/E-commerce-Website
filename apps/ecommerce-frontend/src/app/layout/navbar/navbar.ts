5dfabdff133d9f38aa1302b62dbd0288bb8391d2
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-02 23:43:35 +0100

    update the navbar code

--- apps/ecommerce-frontend/src/app/layout/navbar/navbar.ts
+++ apps/ecommerce-frontend/src/app/layout/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-navbar',
-  imports: [],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
