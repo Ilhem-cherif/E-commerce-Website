8bde803be623070812fe778f863efade7da92801
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-02 23:44:31 +0100

    add some icons

--- apps/ecommerce-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecommerce-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,16 @@
 
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 export const fontAwesomeIcons : IconDefinition[] = [
-  faUser
-
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faFacebook,
+  faTwitter,
 ];
