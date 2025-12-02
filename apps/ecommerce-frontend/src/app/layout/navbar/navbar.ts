e17731b6811fe319a52112ca656bdc50d9d4c7dd
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-02 18:31:38 +0100

    generate the navbar component

---
+++ apps/ecommerce-frontend/src/app/layout/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecom-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {}
